5a93f68f3fd5359814c27fa17e10b68d1bba7bd3
Mainblack <Mainblack@MainblackdeMacBook-Air.local>
2017-10-24 20:50:50 +0800

    sever

--- Server/SERVER.py
+++ Server/SERVER.py
@@ -1,88 +1,66 @@
-#coding=utf-8
+from bottle import *
+import json
 from OnlineJudge import *
 from Search import *
 from Database import *
-from bottle import *
-import json
 
-xoj = Bottle(__name__)
+xoj = Bottle()
 hdu = HDU()
-search_code = search('HDU','1001','3')
 db = Database()
-##########主页面#########
 
-@xoj.route('/0_index', methods=['GET', 'POST'])
-def home():
-    return template('/Users/mac/XOJ/Server/0_index.html')
+@xoj.route('/index.html')
+def index():
+    return template('/Users/mac/XOJ/Server/index.html')
 
-##########返回题目列表##########
-
-@xoj.route('/list', methods=['GET'])
-def Qlist():
+@xoj.route('/list.html')
+def list_():
     return template('/Users/mac/XOJ/Server/list.html')
 
-
-@xoj.route('/listdata', methods=['GET','POST'])
+@xoj.route('/listdata')
 def listdata():
-
-    lst = json.dumps( hdu.list(3) )
-    #lst = hdu.list(3)
-    #print (lst)
+    lst = json.dumps(hdu.list(3))
     return lst
 
-###########返回题目详情#########
-
-@xoj.route('/problem', methods=['GET'])
+@xoj.route('/problem.html')
 def problem():
     return template('problem.html')
 
-@xoj.route('/problemdata', methods=['GET','POST'])
+@xoj.route('/problemdata')
 def pro_detail():
-    problemID = request.forms.get('problemid')      ###problemID由problem.html提供（用户输入）
-    detail = json.dumps( hdu.problem(problemID) )    ###  problem('problemID')
+    problemID = request.forms.get('problemid') 
+    detail = json.dumps( hdu.problem(problemID) )    
     return detail
 
-
-###########返回代码#############
-@xoj.route('/searchresult',methods=['GET','POST'])
+@xoj.route('/searchresult')
 def search_result():
     return search_code
 
-
-###########保存数据至数据库##############
-
-@xoj.route('/save_list',methods = ['GET'])
+@xoj.route('/save_list')
 def save_list():
-    number = request.forms.get('number')     #number  页面显示的题目数量
+    number = request.forms.get('number')    
     db.__init__()
     problem_list = hdu.list(number)   
     db.save_list(problem_list)
     return 'list has been saved'
 
-
-@xoj.route('/save_problem',methods = ['GET'])
+@xoj.route('/save_problem')
 def save_problem():
-    problemid = request.forms.get('problemid')     #number  页面显示的题目数量
+    problemid = request.forms.get('problemid')
     #db.__init__()
     problem = hdu.problem(problemid)   
     db.save_problem(problem)
     return 'Problem has been saved'
 
-############从数据库提取数据########
-
-@xoj.route('/get_list',methods = ['POST'])
+@xoj.route('/get_list')
 def get_list():
-    number = request.forms.get('number')     #number  页面显示的题目数量
+    number = request.forms.get('number')
     return get_list(number)
 
-
-@xoj.route('/get_problem',methods = ['POST'])
+@xoj.route('/get_problem')
 def get_problem():
     oj = request.forms.get('oj')
-    problemid = request.forms.get('problemid')     #number  页面显示的题目数量
+    problemid = request.forms.get('problemid')
     return get_problem(oj,problemid)
 
-
-###########运行服务器########
 if __name__ == '__main__':
     xoj.run(host='127.0.0.1',port='8080')
