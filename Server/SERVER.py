61487f97019d5223f29dcdb213b190e218b454da
Mainblack <Mainblack@MainblackdeMacBook-Air.local>
2017-10-26 20:18:50 +0800

    waiting for db

--- Server/SERVER.py
+++ Server/SERVER.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 from bottle import *
 import json
 from OnlineJudge import *
@@ -8,6 +9,8 @@ xoj = Bottle()
 hdu = HDU()
 db = Database()
 
+db.__init__()
+
 @xoj.route('/index.html')
 def index():
     return template('/Users/mac/XOJ/Server/index.html')
@@ -15,26 +18,14 @@ def index():
 @xoj.route('/list.html')
 def list_():
     return template('/Users/mac/XOJ/Server/list.html')
+'''
+@xoj.route('/get_list')
+def get_list():
+    number = request.forms.get('number')
+    return get_list(number)
+'''
 
-@xoj.route('/listdata')
-def listdata():
-    lst = json.dumps(hdu.list(3))
-    return lst
-
-@xoj.route('/problem.html')
-def problem():
-    return template('problem.html')
-
-@xoj.route('/problemdata')
-def pro_detail():
-    problemID = request.forms.get('problemid') 
-    detail = json.dumps( hdu.problem(problemID) )    
-    return detail
-
-@xoj.route('/searchresult')
-def search_result():
-    return search_code
-
+'''
 @xoj.route('/save_list')
 def save_list():
     number = request.forms.get('number')    
@@ -42,7 +33,25 @@ def save_list():
     problem_list = hdu.list(number)   
     db.save_list(problem_list)
     return 'list has been saved'
+'''
 
+@xoj.route('/listdata/<num:int>')  #数据库能否返回表中数据个数？？？？
+def listdata(num):
+    lst = json.dumps(hdu.list(num))
+    return lst
+
+@xoj.route('/problem.html')
+def problem():
+    return template('problem.html')
+'''
+@xoj.route('/get_problem')
+def get_problem():
+    oj = request.forms.get('oj')
+    problemid = request.forms.get('problemid')
+    return db.get_problem(oj,problemid)
+'''
+
+'''
 @xoj.route('/save_problem')
 def save_problem():
     problemid = request.forms.get('problemid')
@@ -50,17 +59,21 @@ def save_problem():
     problem = hdu.problem(problemid)   
     db.save_problem(problem)
     return 'Problem has been saved'
+'''
 
-@xoj.route('/get_list')
-def get_list():
-    number = request.forms.get('number')
-    return get_list(number)
+@xoj.route('/problemdata/<oj:path>&<pid:path>')
+def problemdata(oj,pid):
+   # problemID = request.forms.get('problemid') 
+    if(db.get_problem(oj,pid)):
+        detail = db.get_problem(oj,pid)
+    else:
+        detail = hdu.problem(pid)
+        db.save_problem(detail)
+    return detail
 
-@xoj.route('/get_problem')
-def get_problem():
-    oj = request.forms.get('oj')
-    problemid = request.forms.get('problemid')
-    return get_problem(oj,problemid)
+@xoj.route('/searchresult')
+def search_result():
+    return search_code
 
 if __name__ == '__main__':
     xoj.run(host='127.0.0.1',port='8080')
--- Server/test.py
+++ Server/test.py
@@ -1,10 +1,25 @@
-from OnlineJudge import *
+from bottle import *
 import json
+from OnlineJudge import *
 from Search import *
-s = search('hdu','1001','3')
+from Database import *
+
+xoj = Bottle()
 hdu = HDU()
-#@xoj.route('/problemdata', methods=['GET','POST'])
-#def pro_detail():
-detail = json.dumps( hdu.problem('1001') )
-print ( detail )
-print ( s )
+
+'''
+@xoj.route('/listdata/<num:int>')
+def listdata(num):
+    lst = json.dumps(hdu.list(num))
+    return lst
+'''
+
+@xoj.route('/problemdata/<oj:path>&<pid:path>')
+def pro_detail(oj,pid):
+   # problemID = request.forms.get('problemid') 
+    detail = json.dumps( hdu.problem(pid) )    
+    return detail
+
+if __name__ == '__main__':
+    xoj.run(host='127.0.0.1',port='8080')
+
